7836d6735f2ad88732b084c2581e21642a451444
Connor Holly <connorholly11@gmail.com>
2019-09-05 14:29:37 -0400

    added dependencies

--- src/App.js
+++ src/App.js
@@ -1,2 +1,16 @@
 import React from 'react'
+import Display from './components/Display';
+import Dashboard from './components/Dashboard';
 
+const App = () => {
+
+    return(
+        <div>
+            <h1>HELLO WORLD</h1>
+            <Display />
+            <Dashboard />
+        </div>
+    )
+}
+
+export default App;
---
+++ src/components/Dashboard.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const Dashboard = () => {
+
+    return(
+        <h1> I AM DASHBOARD </h1>
+    )
+}
